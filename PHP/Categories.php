7292d5a438cbd30b6f5bd6dcd31417bd984e268c
jackchuggard <jahu9463@colorado.edu>
2018-12-05 16:28:18 -0700

    More edits to cat

--- PHP/Categories.php
+++ PHP/Categories.php
@@ -1,29 +1,29 @@
 //variable is $_GET["category"]
-<!Doctype = html>
-
+<!doctype html>
 <html>
-<body>
-   <h1>
+   <body>
+      <h1>Did this work?</h1>
+      <h1>
 <?php
    $host        = "host = ec2-23-23-110-26.compute-1.amazonaws.com";
    $port        = "port = 5432";
    $dbname      = "dbname = d3scuneimo0uco";
    $credentials = "user = crizzbzbapugiv password=c31154c5b8b6fc32b6499b57032a8a9242e1419fc0b785d6bd94dca146dafb52";
-
    $db = pg_connect( "$host $port $dbname $credentials"  );
-
    if(!$db) {
       echo "Error : Unable to open database\n";
    } else {
       echo "Opened database successfully\n";
    }
 ?>
-</h1>
-<h1>
-   <?php    
-   $cat = $_GET["category"]
-   echo $cat
-   ?>
-   </h1>
-</body>
+      </h1>
+
+      <h1>
+         <?php echo $_GET["category"];?>
+      </h1>
+      
+       <h1>
+       <?php  echo pg_get_result($db, "select charity_name from charity where charity_id = 1"); ?>
+      </h1>
+      </body>
 </html>
