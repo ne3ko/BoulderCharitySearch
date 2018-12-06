8b3f3b8c6bfbf4855ce4047a8d269f81fd54d8a5
jackchuggard <jahu9463@colorado.edu>
2018-12-06 02:56:42 -0700

    another try

--- PHP/Categories.php
+++ PHP/Categories.php
@@ -25,7 +25,7 @@
        <?php
 $query = "SELECT charity_name, phone_number, assets, income, expenses, charity_tag, charity.charity_id FROM charity INNER JOIN financial ON financial.charity_id = charity.charity_id WHERE category_name = " . $_Get["category"];
 
-   $result = pg_query($db, "SELECT charity_name, phone_number, assets, income, expenses, charity_tag, charity.charity_id FROM charity INNER JOIN financial ON financial.charity_id = charity.charity_id WHERE category_name = $1", $_Get["category"]);
+   $result = pg_query($db, $query);
 if (!$result) {
   echo "NOOOOOO.\n";
   exit;
