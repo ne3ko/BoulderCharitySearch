d32421350444cc6d2f91c24cbcf0cf39dd50184b
jackchuggard <jahu9463@colorado.edu>
2018-12-06 07:48:00 -0700

    more bugs

--- PHP/Categories.php
+++ PHP/Categories.php
@@ -14,7 +14,7 @@
       echo "Error : Unable to open database\n";
    } 
    
-   $catagory = "Health";
+   $catagory = 'Health';
 // this makes it specific to the value from the form
    $query = sprintf("SELECT charity_name, phone_number, assets, income, expenses, charity_tag, charity.charity_id FROM charity INNER JOIN financial ON financial.charity_id = charity.charity_id WHERE category_name = '%s'", pg_escape_string($catagory));
 
