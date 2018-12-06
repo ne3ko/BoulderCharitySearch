1446533a01b3bbfa6e64ddeac276a39e609de7fa
Richard Moon <rimo1046@colorado.edu>
2018-12-06 06:16:48 -0700

    Update Results.php

--- HTML/Results.php
+++ HTML/Results.php
@@ -47,14 +47,110 @@
    }
 
 
-				$php_data1=array(
-						array("charity name","phone number"),
-						array("assets",100),
-						array("income",100),
-						array("expenses",10000000)				
+								$php_data1=array(
+						array("Charity Name",$a[0]),
+						array("Phone Number",$a[1]),
+						array("Assets",$a[2]),
+						array("Income",$a[3]),
+						array("Expenses",$a[4])				
 				);
-				$php_name1="charityname";
-				$php_description1="charity description tag or text thing";
+				
+				$php_name1=$a[0];
+				$php_description1=$a[5];
+				
+				$php_data2=array(
+						array("Charity Name",$a[7]),
+						array("Phone Number",$a[8]),
+						array("Assets",$a[9]),
+						array("Income",$a[10]),
+						array("Expenses",$a[11])				
+				);
+				
+				$php_name2=$a[7];
+				$php_description2=$a[12];
+				
+				$php_data3=array(
+						array("Charity Name",$a[14]),
+						array("Phone Number",$a[15),
+						array("Assets",$a[16]),
+						array("Income",$a[17]),
+						array("Expenses",$a[18])				
+				);
+				
+				$php_name3=$a[14];
+				$php_description3=$a[19];
+				
+				$php_data4=array(
+						array("Charity Name",$a[21]),
+						array("Phone Number",$a[22),
+						array("Assets",$a[23]),
+						array("Income",$a[24]),
+						array("Expenses",$a[25])				
+				);
+				
+				$php_name4=$a[21];
+				$php_description4=$a[26];
+				
+				$php_data5=array(
+						array("Charity Name",$a[28]),
+						array("Phone Number",$a[29]),
+						array("Assets",$a[30]),
+						array("Income",$a[31]),
+						array("Expenses",$a[32])				
+				);
+				
+				$php_name5=$a[28];
+				$php_description5=$a[33];
+				$php_data6=array(
+						array("Charity Name",$a[35]),
+						array("Phone Number",$a[36]),
+						array("Assets",$a[37]),
+						array("Income",$a[38]),
+						array("Expenses",$a[39])				
+				);
+				
+				$php_name6=$a[35];
+				$php_description6=$a[40];
+				$php_data7=array(
+						array("Charity Name",$a[42]),
+						array("Phone Number",$a[43]),
+						array("Assets",$a[44]),
+						array("Income",$a[45]),
+						array("Expenses",$a[46])				
+				);
+				
+				$php_name7=$a[42];
+				$php_description7=$a[47];
+				$php_data8=array(
+						array("Charity Name",$a[49]),
+						array("Phone Number",$a[50]),
+						array("Assets",$a[51]),
+						array("Income",$a[52]),
+						array("Expenses",$a[53])				
+				);
+				
+				$php_name8=$a[49];
+				$php_description8=$a[54];
+				$php_data9=array(
+						array("Charity Name",$a[56]),
+						array("Phone Number",$a[57]),
+						array("Assets",$a[58]),
+						array("Income",$a[59]),
+						array("Expenses",$a[60])				
+				);
+				
+				$php_name9=$a[56];
+				$php_description9=$a[61];
+				$php_data10=array(
+						array("Charity Name",$a[63]),
+						array("Phone Number",$a[64]),
+						array("Assets",$a[65]),
+						array("Income",$a[66]),
+						array("Expenses",$a[67])				
+				);
+				
+				$php_name10=$a[63];
+				$php_description10=$a[68];
 		?>
 		
 		<script type="text/javascript">
